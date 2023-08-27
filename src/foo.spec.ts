6beaa6978e839d32d53ed78fea9dd8bebb7176a4
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2023-08-27 19:49:04 +0900

    Testing a function using another fp-ts module

--- src/foo.spec.ts
+++ src/foo.spec.ts
@@ -1,8 +1,15 @@
 import { describe, expect, it } from "vitest";
-import { equalStrings } from "./foo";
+// import { equalStrings } from "./foo";
+import { getValue } from "./foo";
 
-describe("equalStrings", () => {
-  it("二つの文字列が等しいとき true を返す", async () => {
-    expect(equalStrings("foo", "foo")).toBeTruthy();
+// describe("equalStrings", () => {
+//   it("二つの文字列が等しいとき true を返す", async () => {
+//     expect(equalStrings("foo", "foo")).toBeTruthy();
+//   });
+// });
+
+describe("getValue", () => {
+  it("Some でタグづけられたオブジェクトが持つ値を得られる", async () => {
+    expect(getValue({ _tag: "Some", value: 1 })).toBe(1);
   });
 });
--- src/foo.ts
+++ src/foo.ts
@@ -1,4 +1,14 @@
-import * as String from "fp-ts/String";
+// import * as String from "fp-ts/String";
+//
+// export const equalStrings = (s1: string, s2: string): boolean =>
+//   String.Eq.equals(s1, s2);
 
-export const equalStrings = (s1: string, s2: string): boolean =>
-  String.Eq.equals(s1, s2);
+import * as Option from "fp-ts/Option";
+
+export const getValue = <T>(option: Option.Option<T>): T => {
+  if (Option.isSome(option)) {
+    return option.value;
+  } else {
+    throw new Error();
+  }
+};
